cd304be412eb5db7cda9dc42ebdb847b73bd0016
FeniTrada <tradafeni457@gmail.com>
2026-04-19 17:40:09 +0530

    Update Q20_CommandLineException.java

--- Practical Set-6/Q20_CommandLineException.java
+++ Practical Set-6/Q20_CommandLineException.java
@@ -1,1 +1,21 @@
+public class Division {
+    public static void main(String[] args) {
+        try {
+            int numerator = Integer.parseInt(args[0]);
+            int denominator = Integer.parseInt(args[1]);
 
+            int result = numerator / denominator;
+
+            System.out.println("Result: " + result);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please provide two arguments");
+        } catch (NumberFormatException e) {
+            System.out.println("Enter valid integers");
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } finally {
+            System.out.println("Program executed");
+        }
+    }
+}
