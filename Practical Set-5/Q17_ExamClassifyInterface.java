f213919bc25d3dec03887ab784496de25080f229
FeniTrada <tradafeni457@gmail.com>
2026-04-19 16:54:28 +0530

    Create Q17_ExamClassifyInterface.java

---
+++ Practical Set-5/Q17_ExamClassifyInterface.java
@@ -0,0 +1,31 @@
+interface Exam {
+    boolean isPassed(int mark);
+}
+
+interface Classify {
+    String getDivision(double average);
+}
+
+class Result implements Exam, Classify {
+
+    public boolean isPassed(int mark) {
+        return mark >= 40;
+    }
+
+    public String getDivision(double average) {
+        if (average >= 60)
+            return "First Division";
+        else
+            return "Second Division";
+    }
+
+    public static void main(String[] args) {
+        Result r = new Result();
+
+        int mark = 50;
+        double avg = 65;
+
+        System.out.println("Passed: " + r.isPassed(mark));
+        System.out.println("Division: " + r.getDivision(avg));
+    }
+}
