a91aa5e01b3705dff1e87b6cf6c9dc3b82bbc421
FeniTrada <tradafeni457@gmail.com>
2026-04-19 15:25:18 +0530

    Update Q1_DistanceConversion.java

--- Practical Set-1/Q1_DistanceConversion.java
+++ Practical Set-1/Q1_DistanceConversion.java
@@ -1,1 +1,22 @@
+/*
+Practical Set-1
+Question No: 1
+Title: Distance Conversion (meters to feet)
+*/
 
+import java.util.Scanner;
+
+public class Q1_DistanceConversion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in meters: ");
+        double meters = sc.nextDouble();
+
+        double feet = meters * 3.28084;
+
+        System.out.printf("Distance in feet = %.2f\n", feet);
+
+        sc.close();
+    }
+}
